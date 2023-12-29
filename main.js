4d195ca4b0b46f43c50f941cacf470e4ce0161b3
mikegirma82@gmail.com <mikegirma82@gmail.com>
2023-12-29 16:49:35 +0300

    mobile fixes

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ const uiCloseIcon = document.querySelector('.close-icon');
 
 uiMenuOpen.addEventListener('click', ()=> {
     uiNavMenuList.classList.add('active');
+    document.querySelector('body').classList.add('no-scroll')
 })
 
 uiCloseIcon.addEventListener('click', ()=> {
     uiNavMenuList.classList.remove('active')
+    document.querySelector('body').classList.remove('no-scroll')
+
 })
